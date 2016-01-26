efe00e2fd5d8c8c22456e3d181f9a53a25bebe6a
brnagn7 <brnagn7@gmail.com>
2016-01-26 19:10:24 +1100

    First Commit, basic site

---
+++ index.php
@@ -0,0 +1,106 @@
+<?php
+$page_title = "Home – Grains of wheat";
+include('inc/header.php');
+?>
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Tab content -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header" align="center">
+                    Welcome to Grains of Wheat
+                </h1>
+            </div>
+<div class="col-md-4">
+<div class="panel panel-danger" style="max-width: 400px;">
+<div class="panel-heading">
+<h3 class="panel-title">Local System Administration <img src="webroot/img/tux2.png" class="img-rounded"></h3>
+<h5><span align="right" class="glyphicon glyphicon-book"></span> Intermediate
+</div>
+<div class="panel-body">
+<p>System Admin Skills</p>
+<p>"In the beginning, the file was without form, and void; and emptiness was upon the face of the bits. And the Fingers of the Author moved upon the face of the keyboard. And the Author said, Let there be words, and there were words." - <a href="http://www.tldp.org/LDP/sag/html/intro.html">LSAG</a></p>
+<a class="btn btn-success navbar-btn" data-method="get" href="localsdm.php">Read more...</a>
+</div>
+</div>
+</div>
+<div class="col-md-4">
+<div class="panel panel-success" style="max-width: 400px;">
+<div class="panel-heading">
+<h3 class="panel-title">Local Security <img src="webroot/img/tux3.png" class="img-rounded"></h3>
+<h5><span align="right" class="glyphicon glyphicon-book"></span> Intermediate
+ </h5>
+</div>
+<div class="panel-body">
+<p>Deny Unauthorized Access</p>
+<p>So you're a Linux System Administrator! But what about the local user's? Hackers seek to gain access to the root account typically via a local user account by escalating privileges. Better learn how to tighten that thing up! - <a href="http://www.linuxsecurity.com/docs/SecurityAdminGuide/SecurityAdminGuide-4.html">LSAG</a></p>
+<a class="btn btn-success navbar-btn" data-method="get" href="localsdm.php">Read more...</a>
+</div>
+</div>
+</div>
+<div class="col-md-4">
+<div class="panel panel-warning" style="max-width: 400px;">
+<div class="panel-heading">
+<h3 class="panel-title">Shell Scripting <img src="webroot/img/tux4.png" class="img-rounded"></h3>
+<h5><span align="right" class="glyphicon glyphicon-book"></span> Intermediate
+ </h5>
+</div>
+<div class="panel-body">
+<p>Automation</p>
+<p>Shell scripts have saved me hours of work, its a great asset to have shell scripting skills so please go ahead and knock the shebang out of it by upskilling with bash.
+</p>
+<p>Over time there will be other automation techniques and I will upload what I have learned in <code>python</code> and <code>c programming</code>.</p>
+<a class="btn btn-success navbar-btn" data-method="get" href="localsdm.php">Read more...</a>
+</div>
+</div>
+</div>
+<div class="clear"></div>
+<div class="col-md-4">
+<div class="panel panel-primary" style="max-width: 400px;">
+<div class="panel-heading">
+<h3 class="panel-title">Software Management <img src="webroot/img/tux5.png" class="img-rounded"></h3>
+<h5><span align="right" class="glyphicon glyphicon-book"></span> Intermediate
+ </h5>
+</div>
+<div class="panel-body">
+<p>Keeping things updated</p>
+<p>How to use package management for updates and patches.
+</p>
+<a class="btn btn-success navbar-btn" data-method="get" href="localsdm.php">Read more...</a>
+</div>
+</div>
+</div>
+<div class="col-md-4">
+<div class="panel panel-info" style="max-width: 400px;">
+<div class="panel-heading">
+<h3 class="panel-title">Command Line <img src="webroot/img/tux6.png" class="img-rounded"></h3>
+<h5><span align="right" class="glyphicon glyphicon-book"></span> Intermediate
+ </h5>
+</div>
+<div class="panel-body">
+<p>Hacking Skills</p>
+<p>Learning how to control and configure your system using only the command line.</p>
+<a class="btn btn-success navbar-btn" data-method="get" href="localsdm.php">Read more...</a>
+</div>
+</div>
+</div>
+<div class="col-md-4">
+<div class="panel panel-danger" style="max-width: 400px;">
+<div class="panel-heading">
+<h3 class="panel-title">Filesystem and Storage <img src="webroot/img/tux7.png" class="img-rounded"></h3>
+<h5><span align="right" class="glyphicon glyphicon-book"></span> Intermediate
+ </h5>
+</div>
+<div class="panel-body">
+<p>Disks</p>
+<p>Encrypting disks, formatting and file system checks.</p>
+<a class="btn btn-success navbar-btn" data-method="get" href="localsdm.php">Read more...</a>
+</div>
+</div>
+</div>
+<hr>
+<!-- Footer -->
+<?php
+include('inc/footer.php');
+?>
